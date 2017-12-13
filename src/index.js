d94f97583c7af6823d7c357585c96beb20be3844
author <author@corpmail.com>
2017-12-13 14:44:43 +0800

    #

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import Routers from './router';
 
 import registerServiceWorker from './registerServiceWorker';
-//页面ant样式没有引入，在这里引入全部的样式，这是一个待解决问题
-// import 'antd/dist/antd.less';
 
 ReactDOM.render(<Routers />, document.getElementById('root'));
 registerServiceWorker();
